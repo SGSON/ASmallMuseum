c9211d98bc6e8f36c81975f87b3716be80de01fd
SG <sons@myumanitoba.ca>
2020-12-23 13:18:55 +0900

    added firebase libs

--- app/src/main/java/sg/asmallmuseum/MainActivity.java
+++ app/src/main/java/sg/asmallmuseum/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference dbRef = db.getReference("message");
+        dbRef.setValue("?????");
     }
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+The purpose of the "A Small Museum" is for non-professional person to share their reviews and arts such as music and drawing to others. This application make easier to understand arts and would be boost culture.
+
+First, users can review to a posting after apreciating. Users will write down their feeling, question and criticism without abuse. 
+Second, artist can check a rating.  
+Third, artists leave the their messages in the composition, so users can easily understand the meaning of the composition.
