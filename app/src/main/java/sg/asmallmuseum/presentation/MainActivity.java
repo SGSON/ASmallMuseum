2d89d71ce692769be4ac61438314d67db44c5814
SG <sons@myumanitoba.ca>
2020-12-26 17:59:17 +0900

    added quick menu layout and icons

--- app/src/main/java/sg/asmallmuseum/presentation/MainActivity.java
+++ app/src/main/java/sg/asmallmuseum/presentation/MainActivity.java
@@ -9,6 +9,10 @@ import sg.asmallmuseum.logic.UserManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -48,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void onMenuButtonPressed(View view) {
         Toast.makeText(this, "Pressed Menu Button", Toast.LENGTH_LONG).show();
+
     }
 
     public void onBackButtonPressed(View view) {
