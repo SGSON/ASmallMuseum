f32477622fc1d2ed21663060a7b626ace12cd309
SG <thstmdrhs123@gmail.com>
2021-01-28 17:00:27 +0900

    resize artview

--- app/src/main/java/sg/asmallmuseum/presentation/ArtView/ArtViewActivity.java
+++ app/src/main/java/sg/asmallmuseum/presentation/ArtView/ArtViewActivity.java
@@ -1,5 +1,6 @@
 package sg.asmallmuseum.presentation.ArtView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -15,8 +16,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -38,6 +41,7 @@ public class ArtViewActivity extends AppCompatActivity implements ManagerListene
     private ArtViewFragmentAdapter pagerAdapter;
     private Button mEnlarge;
     private Button mClose;
+    private Button mMore;
 //    private RecyclerView mPreview;
     private TextView mNumPages;
     private int numImages;
@@ -69,6 +73,9 @@ public class ArtViewActivity extends AppCompatActivity implements ManagerListene
         mClose = (Button) findViewById(R.id.large_close);
         mClose.setOnClickListener(this);
 
+        mMore = (Button) findViewById(R.id.art_more_button);
+        mMore.setOnClickListener(this);
+
         manager.getSingleArtInfoByPath(intent.getStringExtra("DocPath"));
 
         review = new ArrayList<>();
@@ -93,6 +100,41 @@ public class ArtViewActivity extends AppCompatActivity implements ManagerListene
         recyclerView = (RecyclerView)findViewById(R.id.art_scroll_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(reviewAdapter);
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                //Log.d("X:", ""+dy);
+                resizeLayout(dy);
+            }
+        });
+    }
+
+    private void resizeLayout(int dy){
+        ConstraintLayout descLayout = (ConstraintLayout) findViewById(R.id.art_desc_view);
+
+        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) descLayout.getLayoutParams();
+        Log.d("X:", ""+dy);
+        if (params.height > 1 && dy > 5){
+            if (params.height - dy > 0){
+                params.height = params.height - dy;
+            }
+            else {
+                params.height = 1;
+            }
+            descLayout.setLayoutParams(params);
+        }
+        else if (params.height < descLayout.getMaxHeight() && dy < -5){
+            Log.d("X:", params.height - dy+"IN:"+dy);
+            if (params.height - dy < descLayout.getMaxHeight()){
+                    params.height = params.height - dy;
+            }
+            else {
+                params.height = descLayout.getMaxHeight();
+            }
+            descLayout.setLayoutParams(params);
+        }
     }
 
     private void setReview(){
@@ -173,7 +215,24 @@ public class ArtViewActivity extends AppCompatActivity implements ManagerListene
             ConstraintLayout largeView = (ConstraintLayout) findViewById(R.id.art_large_view);
             largeView.setVisibility(View.INVISIBLE);
         }
+        else if(id == R.id.art_more_button){
+            createPopupWindow();
+        }
 
+    }
+
+    private void createPopupWindow(){
+        PopupMenu popupMenu = new PopupMenu(this, mMore);
+        popupMenu.getMenuInflater().inflate(R.menu.menu_more_enable, popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Toast.makeText(ArtViewActivity.this, "a"+menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        popupMenu.show();
     }
 
     /***Enlarge View Methods***/
