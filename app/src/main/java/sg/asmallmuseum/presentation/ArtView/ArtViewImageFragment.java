3468b98f0ad3eb4acd7bc9b1979cdd58c38fba15
SG <thstmdrhs123@gmail.com>
2021-02-26 15:47:08 +0900

    added pinch zoom

--- app/src/main/java/sg/asmallmuseum/presentation/ArtView/ArtViewImageFragment.java
+++ app/src/main/java/sg/asmallmuseum/presentation/ArtView/ArtViewImageFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -15,9 +17,11 @@ import com.bumptech.glide.Glide;
 import sg.asmallmuseum.R;
 
 
-public class ArtViewImageFragment extends Fragment {
+public class ArtViewImageFragment extends Fragment implements View.OnTouchListener {
     private ImageView mImage;
     private Uri uri;
+    private ScaleGestureDetector mScaleGestureDetector;
+    private float mScaleFactor = 1.0f;
 
     public ArtViewImageFragment() {
         // Required empty public constructor
@@ -35,6 +39,11 @@ public class ArtViewImageFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_art_image, container, false);
         mImage = (ImageView) view.findViewById(R.id.fragment_image);
         Glide.with(ArtViewImageFragment.this).load(uri).into(mImage);
+
+        mScaleGestureDetector = new ScaleGestureDetector(getContext(), new ScaleListener());
+        mImage.setOnTouchListener(this);
+
+        
         return view;
     }
 
@@ -52,5 +61,24 @@ public class ArtViewImageFragment extends Fragment {
 
     public Uri getUri(){
         return uri;
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        mScaleGestureDetector.onTouchEvent(motionEvent);
+        return true;
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener{
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            mScaleFactor *= detector.getScaleFactor();
+
+            mScaleFactor = Math.max(0.f, Math.min(mScaleFactor, 10.0f));
+
+            mImage.setScaleX(mScaleFactor);
+            mImage.setScaleY(mScaleFactor);
+            return true;
+        }
     }
 }
